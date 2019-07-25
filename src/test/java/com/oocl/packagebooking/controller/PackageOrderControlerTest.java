b22146a451d434252eb9a45245fa9c68a7ab4fa4
xjhxcc <2351889342@qq.com>
2019-07-25 17:37:37 +0800

    Feat:add test has errror

---
+++ src/test/java/com/oocl/packagebooking/controller/PackageOrderControlerTest.java
@@ -0,0 +1,29 @@
+package com.oocl.packagebooking.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc()
+public class PackageOrderControlerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+//    @Test
+//    public void should_return_packageOrders_when_call_findAllPackageOrder() throws Exception {
+//        mockMvc.perform(MockMvcRequestBuilders.get("/packageOrders")
+//                .accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
+//    }
+
+    
+}
--- src/test/java/com/oocl/packagebooking/controller/PackageOrderTest.java
+++
@@ -1,9 +0,0 @@
-package com.oocl.packagebooking.controller;
-
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-@AutoConfigureMockMvc
-public class PackageOrderTest {
-}
